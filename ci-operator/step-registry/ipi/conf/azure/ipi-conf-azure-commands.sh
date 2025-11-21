0e5fe2860c7266ef46f7d7d2a69be1974e8917a1
Jinyun Ma <jima@redhat.com>
2025-11-21 17:05:28 +0800

    add azure multizone gateway ci jobs

--- ci-operator/step-registry/ipi/conf/azure/ipi-conf-azure-commands.sh
+++ ci-operator/step-registry/ipi/conf/azure/ipi-conf-azure-commands.sh
@@ -110,7 +110,9 @@ EOF
 if [ -z "${OUTBOUND_TYPE}" ]; then
   echo "Outbound Type is not defined"
 else
-  if [ X"${OUTBOUND_TYPE}" == X"UserDefinedRouting" ] || [ X"${OUTBOUND_TYPE}" == X"NATGatewaySingleZone" ] || [ X"${OUTBOUND_TYPE}" == X"NatGateway" ]; then
+  OUTBOUND_TYPE_VALUE="UserDefinedRouting NATGatewaySingleZone NATGatewayMultiZone NatGateway"
+  #shellcheck disable=SC2076
+  if [[ " ${OUTBOUND_TYPE_VALUE} " =~ " ${OUTBOUND_TYPE} " ]]; then
     echo "Writing 'outboundType: ${OUTBOUND_TYPE}' to install-config"
     PATCH="${SHARED_DIR}/install-config-outboundType.yaml.patch"
     cat > "${PATCH}" << EOF
